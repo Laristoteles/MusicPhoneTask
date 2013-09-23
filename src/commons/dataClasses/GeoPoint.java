16822626f303f74d328856e2529ea46478d5424a
slacko <ancestrall@gmail.com>
2013-09-23 16:04:39 +0300

    translated Gps functionalities

---
+++ src/commons/dataClasses/GeoPoint.java
@@ -0,0 +1,23 @@
+package commons.dataClasses;
+
+public class GeoPoint {
+	public GeoPoint(String latitude, String longitude) {
+		super();
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	private String latitude;
+	private String longitude;
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+}
--- src/gps/Gps.java
+++ src/gps/Gps.java
@@ -1,7 +1,35 @@
 package gps;
 
+import commons.dataClasses.GeoPoint;
 import commons.interfaces.IGps;
 
 public class Gps implements IGps{
+	private GeoPoint position;
+	private String distanceUnits;
+	public Gps(){
+		DeviceManager.Instance.Gps = this;
+	}
+	@Override
+	public GeoPoint getCurrentPosition() {
+		return this.position;
+	}
 
+	@Override
+	public String getDistanceUnits() {
+		// TODO Auto-generated method stub
+		return this.distanceUnits;
+	}
+
+	@Override
+	public void setCurrentPosition(GeoPoint value) {
+		this.position=value;
+		
+	}
+
+	@Override
+	public void setDistanceUnits(String value) {
+		this.distanceUnits=value;
+		
+	}
+	
 }
