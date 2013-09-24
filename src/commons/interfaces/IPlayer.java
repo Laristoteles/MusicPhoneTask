e621fcd5f1ba68ddf82d5a13eb844fc6aa5a77af
Iflaah <iflaah.salman@oulu.fi>
2013-09-24 14:27:56 +0300

    IPlayer file conflict removed

--- src/commons/interfaces/IPlayer.java
+++ src/commons/interfaces/IPlayer.java
@@ -1,6 +1,6 @@
 package commons.interfaces;
 
-<<<<<<< HEAD
+
 
 public interface IPlayer {
 	
@@ -8,8 +8,6 @@ public interface IPlayer {
 	void setCurrentTitle(String value);
 	String getCurrentArtist();
 	void setCurrentArtist(String value);
-=======
-public interface IPlayer {
->>>>>>> fdf8fa0c07983e0cf10ed52016077b428033d3c1
+
 
 }
