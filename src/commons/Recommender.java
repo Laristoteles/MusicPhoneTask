240afc3f4d8f829532f98465098d9af7f9df7b7e
slacko <ancestrall@gmail.com>
2013-09-30 16:38:39 +0300

    removed getRecommendation

--- src/commons/Recommender.java
+++ src/commons/Recommender.java
@@ -32,66 +32,18 @@ public class Recommender implements IRecommender {
 
 	@Override
 	public List<Recommendation> getRecommendations() throws Exception {
-		ArrayList<Recommendation> rec = new ArrayList<Recommendation>();
-		String currentArtist = DeviceManager.getInstance().getPlayer().getCurrentArtist();
-		HashMap<String, Integer> artists = new HashMap<String, Integer>();
-		try {
-			List<String> topFans = this.connector.getTopFansForArtist(currentArtist);
-			for (String fan : topFans) {
-				List<String> artist = this.connector.getTopArtistsByFan(fan);
-				for (String art : artist) {
-					if(artists.containsKey(art)) artists.put(art, artists.get(art) + 1);
-					else artists.put(art, 1);
-				}
-			}
-		} catch (Exception e) {
-			System.out.print(e);
-		}
-
-		/*The entries of artists should be desc sorted by value. See commons.utils.ValueCompartor 
-		ValueComparator bvc = new ValueComparator(artists);
-		TreeMap<String, Integer> sortedDict = new TreeMap<String, Integer>(bvc);
-		 */
-
-		for (Entry<String, Integer> entry : artists.entrySet()) {			
-			rec.add(new Recommendation(entry.getKey(), entry.getValue()));
-
-		}
-
-		/* Do the comparison over the ArrayList rather than HashMap. See commons.utils */
-		Collections.sort(rec, new CustomComparator());
-
-		// returns only the top 20 artists
-		return rec.subList(0, 19);
+		throw new SQATException("You need to implement this");
 	}
 
 	@Override
 	public List<Destination> getDestinationsForArtists(String artist) {
-		ArrayList<Destination> result = new ArrayList<Destination>();
-		List<ConcertInfo> concerts;
-		Destination dest=null;
-		GeoPoint currentPosition = DeviceManager.getInstance().getGps().getCurrentPosition();
-		String gpsUnits = DeviceManager.getInstance().getGps().getDistanceUnits();
-
 		try {
-			concerts = this.connector.getConcertsForArtist(artist);
-		} catch (Exception e) {
-			return result;
+			throw new SQATException("You need to implement this!");
+		} catch (SQATException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-
-		for (ConcertInfo concert : concerts) {
-			dest = new Destination(concert);
-
-			try {
-				dest.setDistance(Distance.ComputeDistance(dest.getPosition(), currentPosition, gpsUnits));
-			}
-
-			catch (Exception e) {
-				// TODO: handle exception
-
-			} result.add(dest);
-		}
-		return result;
+		return null;
 	}
 
 	@Override
